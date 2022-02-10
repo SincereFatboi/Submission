2f08b0aab07584dd936d44897b0075f797a635d4
SincereFatboi <ryan.yeo04@gmail.com>
2022-02-10 18:04:07 +0800

    progress

--- Forms.py
+++ Forms.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, EmailField, DateField, PasswordField, IntegerField, validators, ValidationError
+from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, EmailField, DateField, PasswordField, IntegerField, FileField, DecimalField, validators, ValidationError
 # from wtforms.fields.html5 import EmailField
 from wtforms.widgets import PasswordInput
 
@@ -72,6 +72,24 @@ class VendorSignIn(Form):
     username = StringField('Username', [validators.Length(min=1, max=100), validators.DataRequired()])
     email = EmailField('Email', [validators.Length(min=1, max=150), validators.DataRequired()])
     password = PasswordField('Password')
+
+
+class CreateItemForm(Form):
+    image = FileField(u'Image File')
+    name = StringField('Item Name', [validators.Length(min = 1, max = 150), validators.DataRequired()], render_kw={"placeholder": "Enter Item name"})
+    description = TextAreaField('Description', [validators.DataRequired()] ,render_kw={"rows": 10, "cols": 11})
+    rate = DecimalField('Daily Rate', [validators.number_range(min=0),validators.InputRequired()])
+    on_loan = IntegerField('On Loan', [validators.number_range(min=0),validators.InputRequired()])
+    available = IntegerField('Inventory stock', [validators.number_range(min=1),validators.InputRequired()])
+    location = StringField('Pickup/Dropoff location', [validators.Length(min = 1, max = 1000), validators.DataRequired()])
+
+
+class CreateLoanForm(Form):
+    item_pic = FileField(u'Image File')
+    item_name = StringField('Item Name', [validators.Length(min = 1, max = 150), validators.DataRequired()])
+    start_date = DateField('Start Date', format='%Y-%m-%d')
+    end_date = DateField('End Date', format='%Y-%m-%d')
+    customer_name = StringField('Customer Name', [validators.Length(min = 1, max = 150), validators.DataRequired()])
 
 
 
